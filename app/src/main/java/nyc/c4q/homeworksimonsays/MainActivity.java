e8afe85945627ce71a846579210e053da6459013
Ashley Joachim <ashleyjoachim@ac.c4q.nyc>
2017-11-01 19:18:05 -0400

    sound effects added

--- app/src/main/java/nyc/c4q/homeworksimonsays/MainActivity.java
+++ app/src/main/java/nyc/c4q/homeworksimonsays/MainActivity.java
@@ -1,6 +1,7 @@
 package nyc.c4q.homeworksimonsays;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button buttonBlue;
     Button buttonGreen;
     Button buttonYellow;
+    MediaPlayer soundEffectOne;
+    MediaPlayer soundEffectTwo;
+    MediaPlayer soundEffectThree;
+    MediaPlayer soundEffectFour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +31,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonBlue = (Button) findViewById(R.id.blue_button);
         buttonRed = (Button) findViewById(R.id.red_button);
         buttonYellow = (Button) findViewById(R.id.yellow_button);
-
+        soundEffectOne = MediaPlayer.create(MainActivity.this, R.raw.button_effect_one);
+        soundEffectTwo = MediaPlayer.create(MainActivity.this, R.raw.sound_effect_two);
+        soundEffectThree = MediaPlayer.create(MainActivity.this, R.raw.sound_effect_three);
+        soundEffectFour = MediaPlayer.create(MainActivity.this, R.raw.sound_effect_four);
     }
 
 
     public void onClickGreenButton(View v) {
+        soundEffectOne.start();
         buttonGreen = (Button) findViewById(R.id.green_button);
         new CountDownTimer(50, 10) {
             public void onTick(long millisUntilFinished) {
@@ -45,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void onClickRedButton(View v) {
+        soundEffectTwo.start();
         buttonRed = (Button) findViewById(R.id.red_button);
         new CountDownTimer(50, 10) {
             public void onTick(long millisUntilFinished) {
@@ -59,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void onClickBlueButton(View v) {
+        soundEffectThree.start();
         buttonBlue = (Button) findViewById(R.id.blue_button);
         new CountDownTimer(50, 10) {
             public void onTick(long millisUntilFinished) {
@@ -73,6 +84,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void onClickYellowButton(View v) {
+        soundEffectFour.start();
         buttonYellow = (Button) findViewById(R.id.yellow_button);
         new CountDownTimer(50, 10) {
             public void onTick(long millisUntilFinished) {
@@ -91,11 +103,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         CharSequence text = "Simon's turn";
         int duration = Toast.LENGTH_LONG;
         Context context = getApplicationContext();
-        Toast toast = Toast.makeText(context,text,duration);
+        Toast toast = Toast.makeText(context, text, duration);
         toast.show();
         Random random = new Random();
-        int randomButton = random.nextInt(4) + 1;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 4; i++) {
+            int randomButton = random.nextInt(4) + 1;
             switch (randomButton) {
                 default:
                     onClickBlueButton(v);
@@ -111,5 +123,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
             }
         }
+    }
+
+    public void simonTurn() {
+
     }
 }
